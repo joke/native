691f5e83f3ead94c26915372277c395134d90243
Joke de Buhr <joke@xckk.de>
2025-02-15 13:19:19 +0100

    save

---
+++ backend/src/test/java/com/example/demo/NativeApplicationTests.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NativeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+	@Test
+	void contextLoads2() {
+	}
+	@Test
+	void contextLoads3() {
+	}
+
+}
